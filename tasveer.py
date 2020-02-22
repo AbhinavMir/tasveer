deff5a628085a614b9834a9fa7d5e38dc3ac14f5
Abhinav Srivastava <atg271@gmail.com>
2020-02-22 13:08:47 +0530

    Rollback to commit #4

--- tasveer.py
+++ tasveer.py
@@ -1,13 +1,4 @@
-def download(**kwargs):
-    """
-    Downloads images based on query and count. 
-    If you do not include both a query and count, it will prompt utilizing input() 
-    Downloaded to <PWD>/downloads
-    
-    Keyword arguments:
-        query -- image query string
-        count -- number of images to download
-    """
+	def download():
     import os,sys
     class HiddenPrints:
         def __enter__(self):
@@ -17,34 +8,14 @@ def download(**kwargs):
         def __exit__(self, exc_type, exc_val, exc_tb):
             sys.stdout.close()
             sys.stdout = self._original_stdout
-    
-    #check for query and count
-    if not 'query' in kwargs:
-    	query=input("Enter Query: ")
-    else:
-        query = kwargs['query']
-    
-    if not 'count' in kwargs:
-        lim=int(input("Enter number of images: "))
-    else:
-        lim=int(kwargs['count'])
 
-    if not 'width' in kwargs:
-        img_width=input("Enter the width of images: ")
-    else:
-        img_width=kwargs['width']
-
-    if not 'height' in kwargs:
-        img_height=input("Enter the height of images: ")
-    else:
-        img_height=kwargs['height']
-    
+    query=input("Enter Query: ")
+    lim=int(input("Enter number of images: "))
     with HiddenPrints():
         from google_images_download import google_images_download
         #instantiate the class
         response = google_images_download.googleimagesdownload()
-        sizes = img_width + "," + img_height
-        arguments = {"keywords":query,"limit":lim,"exact_size":sizes,"print_urls":True}
+        arguments = {"keywords":query,"limit":lim,"print_urls":True}
         paths = response.download(arguments)
         print("Saved at\n___________________\n")
         for i in range(lim):
